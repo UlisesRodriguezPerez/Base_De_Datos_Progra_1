f0111c066ce681cc4e6b8432f071b3778a53b851
Natalia Vargas <30329240+natalia0911@users.noreply.github.com>
2020-10-19 18:56:14 -0600

    Traveseando

--- CAPA_LOGICA/PROYECTOBD/Controllers/HomeController.cs
+++ CAPA_LOGICA/PROYECTOBD/Controllers/HomeController.cs
@@ -10,19 +10,25 @@ namespace PROYECTOBD.Controllers
     {
         public ActionResult Index()
         {
+            /*View hace referencia a la vista correcpondiente a este action
+             Index, .net entiende que debe retornar la vista index.cshtml*/
             return View();
         }
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "hola desde homecontroller";
 
             return View();
         }
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            /*Es convencion que el nombre de la funcion del actionresult 
+            se llame igual que  su vista, de esta manera sabrá cuál es la 
+            vista, pues todos los action retornar view, pero del nommbre depende
+            a cual view se llame*/
+            ViewBag.Message = "Your contact page. Desde homecontroller";
 
             return View();
         }
