b52894864da05e344187659cfacd69df76500055
Natalia Vargas <30329240+natalia0911@users.noreply.github.com>
2020-10-20 23:13:16 -0600

    SP

--- Base De Datos/Store Procedures/SPActualizarBeneficiarios.sql
+++ Base De Datos/Store Procedures/SPActualizarBeneficiarios.sql
@@ -1,3 +1,9 @@
+-- ==============================================================================
+-- Author:		Natalia Vargas 
+-- Create date: 19/10/2020
+-- Description:	Actualizar un beneficiario en la tabla [dbo].[Beneficiario]
+-- ==============================================================================
+
 USE [BD_TP1_Cuenta_De_Ahorros]
 GO
 CREATE PROCEDURE [dbo].[SPActualizarBeneficiario]
@@ -28,3 +34,4 @@ END
 --LLamar a ejecutar el SP
 --EXEC SPActualizarBeneficiario @pIdTipoParentezco=5,@pPorcentaje=90;
 --SELECT * FROM [dbo].[Beneficiario]
+--DROP PROCEDURE SPActualizarBeneficiario
