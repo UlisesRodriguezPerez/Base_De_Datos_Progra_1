471cc90e166545a505483446bc4120a8e64c4d77
Natalia Vargas <30329240+natalia0911@users.noreply.github.com>
2020-10-25 16:12:54 -0600

    Details,create and controller for Beneficiarios

--- CAPA_LOGICA/PROYECTOBD/Context/Context_Conecction.cs
+++ CAPA_LOGICA/PROYECTOBD/Context/Context_Conecction.cs
@@ -15,35 +15,33 @@ namespace PROYECTOBD.Context
 
         readonly string connectionString = "Data Source=amazon-server.cgalpjkz55ag.us-east-1.rds.amazonaws.com;Initial Catalog=BD_TP1_Cuenta_De_Ahorros;Persist Security Info=True;User ID=UlisesRodriguez;Password=BaseAWS2020";
 
-        public IEnumerable<Beneficiario> GetBeneficiario()
+        public Beneficiario GetBeneficiario(int? IdPersona)  
         {
-            var beneficiarioList = new List<Beneficiario>();
+            var beneficiario = new Beneficiario();
             //Esto para generar la conexion
             using (SqlConnection connection = new SqlConnection(this.connectionString))
             {
                 //Obtener el SP que se va a usar 
                 SqlCommand com = new SqlCommand("SPObtenerBeneficiario", connection);
                 com.CommandType = CommandType.StoredProcedure;
-                connection.Open();
+                //int IdPersona = 1;
+                com.Parameters.AddWithValue("@IdPersona", IdPersona);
 
+                connection.Open();
                 SqlDataReader dr = com.ExecuteReader();
+
                 while (dr.Read())
                 {
-                    var beneficiario = new Beneficiario();
                     beneficiario.IdTipoParentezco = Convert.ToInt32(dr["IdTipoParentezco"].ToString());
                     beneficiario.IdPersona = Convert.ToInt32(dr["IdPersona"].ToString());
                     beneficiario.IdDeCuentaAhorro = Convert.ToInt32(dr["IdDeCuentaAhorro"].ToString());
                     beneficiario.Porcentaje = Convert.ToInt32(dr["Porcentaje"].ToString());
-
-                    beneficiarioList.Add(beneficiario);
                 }
                 //Siempre hay que cerrar la conexion
                 connection.Close();
             }
 
-            return beneficiarioList;
-
-
+            return beneficiario;
         }
 
 
