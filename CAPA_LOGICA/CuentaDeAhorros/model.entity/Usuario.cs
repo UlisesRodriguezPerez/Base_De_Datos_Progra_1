3c1a4684dc2608c585d1ec3114968ac8f28b0175
Ulises R <uli.rp1999@gmail.com>
2020-10-26 04:34:41 -0600

    Nuevo projecto capa lógica.

---
+++ CAPA_LOGICA/CuentaDeAhorros/model.entity/Usuario.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace model.entity
+{
+    public class Usuario
+    {
+        //Creamos los atributos de la clase.
+        private int idUsuario;
+        private string nombreUsuario;
+        private string password;
+        private bool tipoUsuario;
+
+        public Usuario()
+        {
+
+        }
+
+        //Creamos un constructor con la llave primaria.
+        public Usuario(int pIdUsuario)
+        {
+            this.idUsuario = pIdUsuario;
+        }
+
+        //Constructor con todos los campos.
+        public Usuario(int pIdUsuario, string pNombreUsuario, string pPassword, bool pTipoUsuario)
+        {
+            this.idUsuario = pIdUsuario;
+            this.nombreUsuario = pNombreUsuario;
+            this.password = pPassword;
+            this.tipoUsuario = pTipoUsuario;
+        }
+
+        //getters and setters
+        public int IdUsuario { get => idUsuario; set => idUsuario = value; }
+        public string NombreUsuario { get => nombreUsuario; set => nombreUsuario = value; }
+        public string Password { get => password; set => password = value; }
+        public bool TipoUsuario { get => tipoUsuario; set => tipoUsuario = value; }
+    }
+}
