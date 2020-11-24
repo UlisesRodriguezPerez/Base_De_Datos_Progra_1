ca3353317dfb22e0b3008c48af2528943ad2a452
Ulises R <uli.rp1999@gmail.com>
2020-11-24 00:48:52 -0600

    Cuenta Objetivo
    Avance con la implementatción del CRUD de cuenta objetivo.

--- CAPA_LOGICA/CuentaDeAhorros/CuentaDeAhorros/Controllers/CuentaObjetivoController.cs
+++ CAPA_LOGICA/CuentaDeAhorros/CuentaDeAhorros/Controllers/CuentaObjetivoController.cs
@@ -11,17 +11,114 @@ namespace CuentaDeAhorros.Controllers
     {
 
 
-        private static int idEC = 0;
-        private CuentaObjetivoDao objetoEstados;
+        private CuentaObjetivoDao objetoCuentaObjetivo;
 
         public CuentaObjetivoController()
         {
-            objetoEstados = new CuentaObjetivoDao();
+            objetoCuentaObjetivo = new CuentaObjetivoDao();
         }
         // GET: CuentaObjetivo
         public ActionResult Inicio()
         {
             return View();
         }
+
+        public ActionResult Create()
+        {
+            CuentaObjetivo objCuentaObjetivo = new CuentaObjetivo();
+            objetoCuentaObjetivo.find(objCuentaObjetivo);
+            //Pasarle el listado de selectItems a la vista
+
+
+
+
+            return View(objCuentaObjetivo);
+
+        }
+
+        [HttpPost]
+        public ActionResult Create(CuentaObjetivo objCuentaObjetivo)
+        {
+            /**
+             * Recibe el cuentaObjetivo que va a crear
+             */
+            try
+            {
+
+                objetoCuentaObjetivo.create(objCuentaObjetivo);
+                //int numeroCuenta = objCuentaObjetivo.NumeroCuenta;
+                //string id = objetoCuentaObjetivo.findIdCA(objCuentaObjetivo);
+                return RedirectToAction("Inicio/");
+
+
+            }
+            catch
+            {
+                Console.WriteLine("No se pudo agregar");
+                return View();
+            }
+        }
+
+        // GET: CuentaObjetivo/Edit/5
+        [HttpGet]
+        public ActionResult Update(int id)
+        {
+            CuentaObjetivo objCuentaObjetivo = new CuentaObjetivo(id);
+            objetoCuentaObjetivo.find(objCuentaObjetivo);
+            return View(objCuentaObjetivo);
+        }
+
+        // POST: Beneficiarios/Edit/5
+        [HttpPost]
+        public ActionResult Update(CuentaObjetivo objCuentaObjetivo, int ID)
+        {
+            try
+            {
+
+                objCuentaObjetivo.IdCuentaObjetivo = ID;
+                objetoCuentaObjetivo.update(objCuentaObjetivo);
+                //objetoCuentaObjetivo.updatePersona(objCuentaObjetivo);
+                //string id = objetoCuentaObjetivo.findIdCuenta(objCuentaObjetivo);
+                return RedirectToAction("Inicio/" );
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Beneficiarios/Delete/5
+
+        public ActionResult Delete(CuentaObjetivo objCuentaObjetivo, int ID)
+        {
+            //objCuentaObjetivo.IdBeneficiario = ID;
+            objetoCuentaObjetivo.delete(objCuentaObjetivo);
+            //string id = objetoCuentaObjetivo.findIdCuenta(objCuentaObjetivo);
+            return RedirectToAction("Inicio/");
+
+        }
+
+        [HttpGet]
+        public ActionResult Find(int Id)
+        {
+            
+            CuentaObjetivo objCuentaObjetivo = new CuentaObjetivo(Id);
+            objetoCuentaObjetivo.find(objCuentaObjetivo);
+            return View(objCuentaObjetivo);
+        }
+
+        [HttpPost]
+        public ActionResult Find(string ID)
+        {
+            return RedirectToAction("Inicio" + ID);
+        }
+
+        [HttpPost]
+        public ActionResult FindAll(string ID)
+        {
+            return RedirectToAction("Inicio" + ID);
+        }
+
+
     }
 }
--- CAPA_LOGICA/CuentaDeAhorros/model.dao/CuentaObjetivoDao.cs
+++ CAPA_LOGICA/CuentaDeAhorros/model.dao/CuentaObjetivoDao.cs
@@ -21,31 +21,136 @@ namespace model.dao
             objConexion = Conexion.saberEstado();
         }
 
-        public void create(CuentaObjetivo objetoCuentaObjetivo)
+        public void create(CuentaObjetivo objCuentaObjetivo)
         {
+            string existePersona;
+            try
+            {
+                comando = new SqlCommand("CREAR SP", objConexion.getConexion());
+                comando.CommandType = CommandType.StoredProcedure;
+                //comando.Parameters.AddWithValue("@pIdTipoParentezco",objCuentaObjetivo.IdTipoParentezco);
+                comando.Parameters.AddWithValue("@pParentezco", objCuentaObjetivo);
+                comando.Parameters.AddWithValue("@pDocumentoIdentidadPersona", objCuentaObjetivo);
+                //comando.Parameters.AddWithValue("@pIdPersona", objCuentaObjetivo.IdPersona);
+                comando.Parameters.AddWithValue("@pNumeroCuentaAhorro", objCuentaObjetivo);
+                comando.Parameters.AddWithValue("@pPorcentaje ", objCuentaObjetivo);
+                objConexion.getConexion().Open();
+                comando.ExecuteNonQuery();
 
+            }
+
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                objConexion.getConexion().Close();
+                objConexion.cerrarConexion();
+            }
+            //return existePersona;
         }
 
-
-        public void delete(CuentaObjetivo objetoCuentaObjetivo)
+        public void delete(CuentaObjetivo objCuentaObjetivo)
         {
-
+            try
+            {
+                comando = new SqlCommand("CREAR SP", objConexion.getConexion());
+                comando.CommandType = CommandType.StoredProcedure;
+                comando.Parameters.AddWithValue("@Id", objCuentaObjetivo.IdCuentaObjetivo);
+                objConexion.getConexion().Open();
+                comando.ExecuteNonQuery();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                objConexion.getConexion().Close();
+                objConexion.cerrarConexion();
+            }
         }
 
-        public bool find(CuentaObjetivo objetoCuentaObjetivo)
+        public bool find(CuentaObjetivo objCuentaObjetivo)
         {
-            return true;
+            bool hayRegistros;
+            try
+            {
+                comando = new SqlCommand("CREAR SP", objConexion.getConexion());
+                comando.CommandType = CommandType.StoredProcedure;
+                comando.Parameters.AddWithValue("@Id", objCuentaObjetivo.IdCuentaObjetivo);
+                objConexion.getConexion().Open();
+                SqlDataReader read = comando.ExecuteReader();
+                hayRegistros = read.Read();
+                if (hayRegistros)
+                {
+                    //objCuentaObjetivo.IdBeneficiario = Convert.ToInt32(read[0].ToString());
+                    //objCuentaObjetivo.IdPersona = Convert.ToInt32(read[1].ToString());
+                    //objCuentaObjetivo.IdTipoParentezco = Convert.ToInt32(read[2].ToString());
+                    //objCuentaObjetivo.IdCuenta = Convert.ToInt32(read[3].ToString());
+                    //objCuentaObjetivo.Porcentaje = Convert.ToInt32(read[4].ToString());
+                    //objCuentaObjetivo.Nombre = read[5].ToString();
+                    //objCuentaObjetivo.Parentezco = read[6].ToString();
+                    //objCuentaObjetivo.NumeroCuenta = Convert.ToInt32(read[7].ToString());
+                    //objCuentaObjetivo.FechaNacimiento = Convert.ToDateTime(read[8].ToString());
+                    //objCuentaObjetivo.Telefono1 = read[9].ToString();
+                    //objCuentaObjetivo.Telefono2 = read[10].ToString();
+                    //objCuentaObjetivo.CorreoElectronico = read[11].ToString();
+                    //objCuentaObjetivo.ValorDocumentoId = read[12].ToString();
+
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                objConexion.getConexion().Close();
+                objConexion.cerrarConexion();
+            }
+            return hayRegistros;
         }
+
         public List<CuentaObjetivo> findAll()
         {
-            List<CuentaObjetivo> listaCuentaObjetivos = new List<CuentaObjetivo>();
-            return listaCuentaObjetivos;
+            List<CuentaObjetivo> listaCuentaObjetivo = new List<CuentaObjetivo>();
+            return listaCuentaObjetivo;
         }
 
-        public void update(CuentaObjetivo objetoCuentaObjetivo)
+        public void update(CuentaObjetivo objCuentaObjetivo)
         {
-
+            try
+            {
+                comando = new SqlCommand("CREAR SP", objConexion.getConexion());
+                comando.CommandType = CommandType.StoredProcedure;
+                comando.Parameters.AddWithValue("@pIdBeneficiario", objCuentaObjetivo);
+                comando.Parameters.AddWithValue("@pIdPersona", objCuentaObjetivo);
+                comando.Parameters.AddWithValue("@pParentezco", objCuentaObjetivo);
+                comando.Parameters.AddWithValue("@pIdCuenta", objCuentaObjetivo);
+                comando.Parameters.AddWithValue("@pPorcentaje", objCuentaObjetivo);
+                comando.Parameters.AddWithValue("@pNombre", objCuentaObjetivo);
+                comando.Parameters.AddWithValue("@pValorDocumentoIdentidad", objCuentaObjetivo);
+                comando.Parameters.AddWithValue("@pFechaNacimiento", objCuentaObjetivo);
+                comando.Parameters.AddWithValue("@pTelefono1", objCuentaObjetivo);
+                comando.Parameters.AddWithValue("@pTelefono2", objCuentaObjetivo);
+                comando.Parameters.AddWithValue("@pCorreoElectronico", objCuentaObjetivo);
+                objConexion.getConexion().Open();
+                comando.ExecuteNonQuery();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                objConexion.getConexion().Close();
+                objConexion.cerrarConexion();
+            }
         }
+
+
 
     }
 }
