fbbcfa075c0578a28efa600c1c9322c8d9104b8e
Ulises R <uli.rp1999@gmail.com>
2020-12-01 03:18:52 -0600

    Nuevos SP
    Se implementaron nuevos SP para la simulación.

--- Base De Datos/Types/User-Defined Table Type/dbo.TableMovimientos.sql
+++ Base De Datos/Types/User-Defined Table Type/dbo.TableMovimientos.sql
@@ -1,19 +1,14 @@
 USE BD_TP1_Cuenta_De_Ahorros
 GO
 
-
-IF type_id('[dbo].[TableMovimiento]') IS NOT NULL
-        DROP TYPE [dbo].[TableMovimiento]; 
-
-CREATE TYPE TableMovimiento AS TABLE
+IF type_id('[dbo].[CuentaAhorroPerTrigger]') IS NOT NULL
+        DROP TYPE [dbo].[CuentaAhorroPerTrigger]; 
+CREATE TYPE CuentaAhorroPerTrigger AS TABLE
 		(
 			 id iNT IDENTITY(1,1) 
-			,idTipoMovimiento INT
+			,idPersona INT
+			,idTipoCuenta INT
 			,numeroCuenta INT
-			,monto MONEY
-			,fecha DATE
-			,descripcion VARCHAR(100)
-		)
-
-
-		
+			,fechaCreacion DATE
+			,saldo MONEY
+		)		
