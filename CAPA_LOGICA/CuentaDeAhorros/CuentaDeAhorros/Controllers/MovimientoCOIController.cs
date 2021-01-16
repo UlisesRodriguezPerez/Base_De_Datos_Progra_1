463f31411f67aa847291b43023895a797cc23e49
JosueNavarroSolis <56096921+JosueNavarroSolis@users.noreply.github.com>
2021-01-15 23:10:54 -0600

    MovimientoCO interes

---
+++ CAPA_LOGICA/CuentaDeAhorros/CuentaDeAhorros/Controllers/MovimientoCOIController.cs
@@ -0,0 +1,36 @@
+﻿using model.dao;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using model.entity;
+
+namespace CuentaDeAhorros.Controllers
+{
+    public class MovimientoCOIController : Controller
+    {
+
+
+        private static int idEC = 0;
+        private MovimientoCOIDao objetoMovimientoCOI;
+
+        public MovimientoCOIController()
+        {
+            objetoMovimientoCOI = new MovimientoCOIDao();
+        }
+
+        [HttpGet]
+        public ActionResult Inicio(int Id)
+        {
+            List<MovimientoCo> lista = objetoMovimientoCOI.buscarMoviminetosCOI(Id);
+            return View(lista);
+        }
+
+        //[HttpGet]
+        //public ActionResult Create()
+        //{
+        //    return View();
+        //}
+    }
+}
---
+++ CAPA_LOGICA/CuentaDeAhorros/model.entity/MovimientoCOI.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace model.entity
+{
+    public class MovimientoCOI
+    {
+        private int idMovimientoCOI;
+        private int idCuentaObjetivo;
+        private DateTime fecha;
+        private string tipoMovimientoCOI;
+        private int idTipoMovimientoCOI;
+        private decimal monto;
+        private decimal nuevoMonto;
+        private int idCuentaAhorro;
+        private string numeroCuentaAhorro;
+        private string descripcion;
+        private int numeroCuentaObjetivo;
+
+
+
+        public MovimientoCOI() { }
+
+        public MovimientoCOI(int id)
+        {
+            this.idMovimientoCOI = id;
+        }
+        public MovimientoCOI(int id, int idCuentaObjetivo, int idCuentaAhorro, int idTipoMovimientoCOI, string pNumeroCuentaAhorro, DateTime fecha, string tipoMovimientoCOI, decimal monto, decimal nuevoMonto, string descripcion, int numeroCuentaObjetivo)
+        {
+            this.idMovimientoCOI = id;
+            this.idCuentaAhorro = idCuentaAhorro;
+            this.idCuentaObjetivo = idCuentaObjetivo;
+            this.idTipoMovimientoCOI = idTipoMovimientoCOI;
+            this.numeroCuentaAhorro = pNumeroCuentaAhorro;
+            this.fecha = fecha;
+            this.tipoMovimientoCOI = tipoMovimientoCOI;
+            this.monto = monto;
+            this.nuevoMonto = nuevoMonto;
+            this.descripcion = descripcion;
+            this.numeroCuentaObjetivo = numeroCuentaObjetivo;
+        }
+
+        //Getters and Setters
+        public int IdMovimientoCOI { get => idMovimientoCOI; set => idMovimientoCOI = value; }
+        public int IdCuentaAhorro { get => idCuentaAhorro; set => idCuentaAhorro = value; }
+        public string NumeroCuentaAhorro { get => numeroCuentaAhorro; set => numeroCuentaAhorro = value; }
+        public int IdTipoMovimientoCOI { get => idTipoMovimientoCOI; set => idTipoMovimientoCOI = value; }
+        public int IdCuentaObjetivo { get => idCuentaObjetivo; set => idCuentaObjetivo = value; }
+        public DateTime Fecha { get => fecha; set => fecha = value; }
+        public string TipoMovimientoCOI { get => tipoMovimientoCOI; set => tipoMovimientoCOI = value; }
+        public decimal Monto { get => monto; set => monto = value; }
+        public decimal NuevoMonto { get => nuevoMonto; set => nuevoMonto = value; }
+        public string Descripcion { get => descripcion; set => descripcion = value; }
+        public int NumeroCuentaObjetivo { get => numeroCuentaObjetivo; set => numeroCuentaObjetivo = value; }
+    }
+}
