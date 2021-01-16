8bcd1b204da1cfdc12fbfc74de6b7358ac60b983
JosueNavarroSolis <56096921+JosueNavarroSolis@users.noreply.github.com>
2021-01-15 23:15:41 -0600

    Movimiento COIDao

---
+++ CAPA_LOGICA/CuentaDeAhorros/model.dao/MovimientoCOIDao.cs
@@ -0,0 +1,66 @@
+﻿using model.entity;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace model.dao
+{
+    public class MovimientoCOIDao
+    {
+        private Conexion objConexion;
+        private SqlCommand comando;
+
+
+        public MovimientoCOIDao()
+        {
+            objConexion = Conexion.saberEstado();
+        }
+        public List<MovimientoCOI> buscarMoviminetosCOI(int id)
+        {
+            List<MovimientoCOI> listaMovimientosCOI = new List<MovimientoCOI>();
+            try
+            {
+                comando = new SqlCommand("SPObtenerMovimientosCOI", objConexion.getConexion());
+                comando.CommandType = CommandType.StoredProcedure;
+                comando.Parameters.AddWithValue("@pIdCuentaObjetivo", id);
+                objConexion.getConexion().Open();
+                SqlDataReader read = comando.ExecuteReader();
+                while (read.Read())
+                {
+                    MovimientoCOI objetoMovimientoCOI = new MovimientoCOI
+                    {
+
+                        IdCuentaObjetivo = Convert.ToInt32(read[0].ToString()),
+                        IdCuentaAhorro = Convert.ToInt32(read[1].ToString()),
+                        NumeroCuentaObjetivo = Convert.ToInt32(read[2].ToString()),
+                        Fecha = Convert.ToDateTime(read[3].ToString()),
+                        Monto = Convert.ToDecimal(read[4].ToString()),
+                        NuevoMonto = Convert.ToDecimal(read[5].ToString()),
+                        Descripcion = read[6].ToString(),
+                        NumeroCuentaAhorro = read[7].ToString(),
+
+                    };
+                    listaMovimientosCOI.Add(objetoMovimientoCOI);
+                }
+
+            }
+
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                objConexion.getConexion().Close();
+                objConexion.cerrarConexion();
+            }
+            return listaMovimientosCOI;
+        }
+    }
+}
+
+
